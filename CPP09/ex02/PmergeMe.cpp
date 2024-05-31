4e4d8f8b6ce3f72ec0c72b42f80084953b30b30a
SamueleGalli <samuele.aggeggio@gmail.com>
2024-05-31 15:04:55 +0200

    sorting

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -53,54 +53,66 @@ int check_error(char **v, int c)
     return (1);
 }
 
+
+// valori disapri tipo 3 5 9 7 4 ma anche pari duplica valore e cancella altri
 template <typename T>
-void    insertionsort(T &vectoring, int left, int right)
+void    insertionsort(T &vectoring, int start, int end)
 {
-   for (int i = left + 1; i <= right; ++i) {
+    // Itero dall'inizio (start + 1) fino alla fine (end)
+    int i = start + 1;
+    if (i <= end)
+    {
+        // Ottengo l'elemento corrente
         char *key = vectoring[i];
         int j = i - 1;
 
-        while (j >= left && strcmp(vectoring[j], key) > 0) {
+        std::cout << atoi(vectoring[i]) << " < " << atoi(key) << std::endl;
+        // Ciclo finché non raggiungo l'inizio dell'array o trovo un elemento minore di key
+        if (std::atoi(vectoring[j]) > std::atoi(key))
+        {
+            // Sposto gli elementi maggiori di key a destra
             vectoring[j + 1] = vectoring[j];
             --j;
         }
+
+        // Inserisco key nella sua posizione corretta
         vectoring[j + 1] = key;
     }
 }
 
-//indice inizio vettore (left)
-//indice fine vettore (right)
+//indice inizio vettore (start)
+//indice fine vettore (end)
 //indice del mezzo della porzione (middle)
 template <typename T>
-void    merge(T &vectoring, int left, int middle, int right)
+void    merge(T &vectoring, int start, int middle, int end)
 {
 
-    int num1 = middle - left + 1;
-    int num2 = right - middle;
-    //inizializzo due sottovettori L e R
-    std::vector<char *> L(num1), R(num2);
+    int num1 = middle - start + 1;
+    int num2 = end - middle;
 
+    //inizializzo due sottovettori L e R
+    T L(num1), R(num2);
     //copio gli elementi del sottovettore in L verso la meta
     for (int i = 0; i < num1; i++)
-        L[i] = vectoring[left + i];
+        L[i] = vectoring[start + i];
     //copio gli elementi del sottovettore in R verso la fine
     for (int j = 0; j < num2; j++)
         R[j] = vectoring[middle + 1 + j];
-    int i = 0, j = 0, k = left;
+    int i = 0, j = 0, k = start;
     while (i < num1 && j < num2)
     {
+        if (R[0] == 0)
+            break ;
         //se il primo elemento di L è minore o uguale al primo elemento di R
-        if (strcmp(L[i], R[j]) <= 0)
+        if (std::atoi(L[i]) <= std::atoi(R[j]))
         {
             //copio l'elemento di sinistra nel vettore che sto creando
-            vectoring[k] = L[i];
-            i++;
+            vectoring[k] = L[i++];
         }
         else
         {
             //copio l'elemento di destra nel vettore che sto creando
-            vectoring[k] = R[j];
-            j++;
+            vectoring[k] = R[j++];
         }
         k++;
     }
@@ -111,64 +123,58 @@ void    merge(T &vectoring, int left, int middle, int right)
         vectoring[k++] = R[j++];
 }
 
-//indice inizio sottovettore (left)
-//indice fine sottovettore (right)
+//indice inizio sottovettore (start)
+//indice fine sottovettore (end)
 template <typename T>
-void    mergesortinsert(T &vectoring, int left, int right)
+void    mergesortinsert(T &vectoring, int start, int end)
 {
-    if (left < right)
-    {
-        int middle = left + (right - left) / 2;
-        //eseguo algoritmo tra inizio e meta vettore
-        mergesortinsert(vectoring, left, middle);
-        //eseguo algoritmo tra meta + 1 e fine vettore
-        mergesortinsert(vectoring, middle + 1, right);
-        //fondo
-        merge(vectoring, left, middle, right);
-    }
+    //se sono a 2 elementi li ordino
+    if ((end - start) + 1 < 2)
+        insertionsort(vectoring, start, end);
     else
-        insertionsort(vectoring, left, right);
-}
-
-long long    get_time(void)
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (static_cast<long long>(ts.tv_sec) * 1000LL + static_cast<long long>(ts.tv_nsec) / 1000000LL);
-}
-
-long long getCurrentTimeMillis() {
-    time_t currentTime;
-    struct tm *timeInfo;
-    time(&currentTime);
-    timeInfo = localtime(&currentTime);
-    return (timeInfo->tm_hour * 3600000) + (timeInfo->tm_min * 60000) + (timeInfo->tm_sec * 1000);
+    {
+        //se inizio e diverso da fine
+        if (start < end)
+        {
+            int middle = start + (end - start) / 2;
+            //eseguo algoritmo tra inizio e meta vettore
+            mergesortinsert(vectoring, start, middle);
+            //eseguo algoritmo tra meta + 1 e fine vettore
+            mergesortinsert(vectoring, middle + 1, end);
+            //fondo
+            merge(vectoring, start, middle, end);
+        }
+    }
 }
 
 void    sorting(char **v, int c)
 {
     //parto da 1 cosi da evitare il programma
     //evito la fine dell'array con v[c - 1]
+
+    //tolgo il programma dal sorting
+    for (int i = 1; i < c; ++i)
+        v[i - 1] = v[i];
     std::vector<char *> vectoring(v, v + c);
     std::deque<char *> dequeing(v, v + c);
-
-
-    long long startTime = getCurrentTimeMillis();
     mergesortinsert(vectoring, 0, vectoring.size() - 1);
-    long long endTime = getCurrentTimeMillis();
-    long long duration_vectoring = endTime - startTime;
-    std::cout << "Time to process a range of " << c - 1<< " elements with std::vector :[..] : " << duration_vectoring << " us" << std::endl;
-
-    startTime = getCurrentTimeMillis();
     mergesortinsert(dequeing, 0, dequeing.size() - 1);
-    endTime = getCurrentTimeMillis();
-    long long duration_dequeing = endTime - startTime;
-    std::cout << "Time to process a range of " << c - 1<< " elements with std::deque :[..] : " << duration_dequeing << " us" << std::endl;
     
-    
-    std::cout << "After:  ";
+    std::cout << "\nAfter:  ";
     int i = 0;
     for (std::vector<char *>::iterator it = vectoring.begin(); it != vectoring.end(); it++)
+    {
+        if (i == 0)
+        {
+            i++;
+            continue;
+        }
+        std::cout << *it << " ";
+    }
+
+    std::cout << "\nAfter:  ";
+    i = 0;
+    for (std::deque<char *>::iterator it = dequeing.begin(); it != dequeing.end(); it++)
     {
         if (i == 0)
         {
